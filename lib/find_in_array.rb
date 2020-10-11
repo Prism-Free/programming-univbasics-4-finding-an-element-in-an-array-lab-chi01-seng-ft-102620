d40df2ca361786c8c34a98df4695c82aa41f9d7d
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-11 00:11:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,3 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  
-  if array.include?(value_to_find)
-    until array[counter] ==
-    
-    end
-  end
+  array.index(value_to_find)
 end
